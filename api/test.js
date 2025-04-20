47ace1850d8e2ff648003b3eda245ecee01c280e
Flashx123 <sovit306@gmail.com>
2025-04-21 02:06:56 +0530

    this is the commit of using the cors

--- api/test.js
+++ api/test.js
@@ -1,7 +1,10 @@
 import express from 'express';
+// const cors = require('cors');
+import cors from 'cors';
 const app = express();
 
 //const PORT = 3000;
+app.use(cors());
 app.get('/api',(req,res)=>{
     res.json("this is it");
 });
